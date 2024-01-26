70608ce0fac2b4575f2b3d03c781a1dbdc0bacc0
Yegor Pokrovskiy <lunavod@bk.ru>
2024-01-26 23:08:48 +0200

    feat(kq): prisma integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly userService: UserService,) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<User[]> {
+    return this.userService.users({});
   }
 }
